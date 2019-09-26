7aabef46df38841c69beb73b44a76202abeba104
nic0lasfara <nicolas.farabegoli@gmail.com>
2019-09-26 10:42:01 +0200

    Draft

--- test-complex/main.cu
+++ test-complex/main.cu
@@ -1,21 +1,172 @@
 #include <iostream>
 #include <stdio.h>
 #include <mma.h>
+#include <cuComplex.h>
 
 #define WARP_SIZE 32
+#define BLKSIZE 1024
 
 using namespace nvcuda;
 
-__global__ void dot_wmma4x4(half *a, half *b, half *c)
+__global__ void dot_wmma16x16(half *a, half *b, float *c)
 {
   wmma::fragment<wmma::matrix_a, 16, 16, 16, half, wmma::row_major> a_frag;
   wmma::fragment<wmma::matrix_b, 16, 16, 16, half, wmma::row_major> b_frag;
-  wmma::fragment<wmma::accumulator, 16, 16, 16, half> c_frag;
+  wmma::fragment<wmma::accumulator, 16, 16, 16, float> c_frag;
   wmma::load_matrix_sync(a_frag, a, 16);
   wmma::load_matrix_sync(b_frag, b, 16);
   wmma::fill_fragment(c_frag, 0.0f);
   wmma::mma_sync(c_frag, a_frag, b_frag, c_frag);
   wmma::store_matrix_sync(c, c_frag, 16, wmma::mem_row_major);
+}
+
+__global__ void mat_sub(half *a, half *b, half *res, const unsigned size)
+{
+  const unsigned i = threadIdx.x + blockDim.x * blockIdx.x;
+  if (i < size) {
+    res[i] = a[i] - b[i];
+  }
+}
+
+__global__ void mat_add(half *a, half *b, half *res, const unsigned size)
+{
+  const unsigned i = threadIdx.x + blockDim.x * blockIdx.x;
+  if (i < size) {
+    res[i] = a[i] + b[i];
+  }
+}
+
+/*__global__ void matrix_padding_16x16(cuDoubleComplex *a, half *re, half *im)
+{
+  const unsigned i = threadIdx.x + blockIdx.x * blockDim.x;
+  const unsigned j = threadIdx.y + blockIdx.y * blockDim.y;
+  if (i < 16 && j < 16) {
+    if (i < 3 && j < 3){
+      re[j + 3*i] = __float2half((float) cuCreal(a[j + 3*i]));
+      im[j + 3*i] = __float2half((float) cuCimag(a[j + 3*i]));
+    } else {
+      re[j + 3*i] = __float2half(0.0f);
+      im[j + 3*i] = __float2half(0.0f);
+    }
+  }
+}
+
+__global__ void vector_padding_16x16(cuDoubleComplex *a, half *re, half *im)
+{
+  const unsigned i = threadIdx.x + blockIdx.x * blockDim.x;
+  const unsigned j = threadIdx.y + blockIdx.y * blockDim.y;
+  if (i < 16 && j < 16) {
+    if (i < 3 && j < 1) {
+      re[j + 3*i] = __float2half((float) cuCreal(a[j + 3*i]));
+      im[j + 3*i] = __float2half((float) cuCimag(a[j + 3*i]));
+    } else {
+      re[j + 3*i] = __float2half(0.0f);
+      im[j + 3*i] = __float2half(0.0f);
+    }
+  }
+}*/
+
+__host__ __device__ static inline half* IDX(half *m, unsigned i, unsigned j, unsigned size)
+{
+  return m + (j + i*size);
+}
+
+__global__ void fill_zero(half *re, half *im, float *c)
+{
+  const unsigned i = threadIdx.x + blockIdx.x * blockDim.x;
+  const unsigned j = threadIdx.y + blockIdx.y * blockDim.y;
+
+  if (i < 16 && j < 16) {
+    re[j + 16*i] = __float2half(0.0f);
+    im[j + 16*i] = __float2half(0.0f);
+    c[j + 16*i] = 0.0f;
+  }
+}
+
+__host__ __device__ static inline void _sub_matrix_real(half *m, cuDoubleComplex *a)
+{
+#pragma unroll
+  for (unsigned i = 0; i < 3; i++)
+#pragma unroll
+    for (unsigned j = 0; j < 3; j++) {
+      *IDX(m, i, j, 3) = __float2half((float) cuCreal(a[j + i*3]));
+    }
+}
+
+__host__ __device__ static inline void _sub_matrix_imag(half *m, cuDoubleComplex *a)
+{
+#pragma unroll
+  for (unsigned i = 0; i < 3; i++)
+#pragma unroll
+    for (unsigned j = 0; j < 3; j++) {
+      *IDX(m, i, j, 3) = __float2half((float) cuCimag(a[j + i*3]));
+    }
+}
+
+__host__ __device__ static inline void _sub_vec_real(half *v, cuDoubleComplex *a)
+{
+#pragma unroll
+  for (unsigned i = 0; i < 3; i++)
+    *IDX(v, i, 0, 3) = __float2half((float) cuCreal(a[i]));
+}
+
+__host__ __device__ static inline void _sub_vec_imag(half *v, cuDoubleComplex *a)
+{
+#pragma unroll
+  for (unsigned i = 0; i < 3; i++)
+    *IDX(v, i, 0, 3) = __float2half((float) cuCimag(a[i]));
+}
+
+__global__ void compose_matrix(half *t_mat, half *t_vec, cuDoubleComplex *mat, cuDoubleComplex *vec)
+{
+  const unsigned gi = threadIdx.x + blockIdx.x * blockDim.x;
+  if (gi == 0)   _sub_matrix_real(t_mat, mat);
+  if (gi == 51)  _sub_matrix_imag(t_mat + 51, mat);
+  if (gi == 86)  _sub_matrix_real(t_mat + 86, mat);
+  if (gi == 137) _sub_matrix_imag(t_mat + 137, mat);
+
+  if (gi == 0)   _sub_vec_real(t_vec, vec);
+  if (gi == 51)  _sub_vec_imag(t_vec + 51, vec);
+  if (gi == 86)  _sub_vec_real(t_vec + 86, vec);
+  if (gi == 137) _sub_vec_imag(t_vec + 137, vec);
+}
+
+void complex_mma(cuDoubleComplex *mat, cuDoubleComplex *vec, const unsigned size)
+{
+  const size_t mat_size = 16 * 16;
+
+  half *t_mat;
+  half *t_vec;
+  float *t_res;
+
+  cudaMalloc((void **)& t_mat, sizeof(half) * mat_size);
+  cudaMalloc((void **)& t_vec, sizeof(half) * mat_size);
+  cudaMalloc((void **)& t_res, sizeof(float) * mat_size);
+
+  dim3 blockDim(BLKSIZE, BLKSIZE);
+  dim3 gridDim((16 + BLKSIZE - 1) / 16, (16 + BLKSIZE - 1) / 16);
+
+  fill_zero<<<gridDim, blockDim>>>(t_mat, t_vec, t_res);
+  compose_matrix<<<1, mat_size>>>(t_mat, t_vec, mat, vec);
+  dot_wmma16x16<<<1, WARP_SIZE>>>(t_mat, t_vec, t_res);
+
+  float *p_res = (float *) malloc(sizeof(float) * mat_size);
+  cudaMemcpy(p_res, t_res, sizeof(float) * mat_size, cudaMemcpyDeviceToHost);
+
+  for (unsigned i = 0; i < 16; i++){
+    for (unsigned j = 0; j < 16; j++){
+      printf("%.2f ", p_res[j + 16*i]);
+    }
+    printf("\n");
+  }
+
+  // Padding matrix to be used with tensor core
+  //matrix_padding_16x16<<<gridDim, blockDim>>>(a, a_re, a_im);
+  //vector_padding_16x16<<<gridDim, blockDim>>>(b, b_re, b_im);
+
+  cudaFree(t_mat);
+  cudaFree(t_vec);
+  cudaFree(t_res);
 }
 
 __global__ void fill_matrix(half *m, const unsigned size)
--- test-complex/main.cudafe1.stub.c
+++ test-complex/main.cudafe1.stub.c
@@ -8,6 +8,7 @@
 #include "crt/host_runtime.h"
 #include "main.fatbin.c"
 extern void __device_stub__Z11dot_wmma4x4P6__halfS0_S0_(half *, half *, half *);
+extern void __device_stub__Z7mat_subP6__halfS0_S0_j(half *, half *, half *, const unsigned);
 extern void __device_stub__Z11fill_matrixP6__halfj(half *, const unsigned);
 static void __nv_cudaEntityRegisterCallback(void **);
 static void __sti____cudaRegisterAll(void) __attribute__((__constructor__));
@@ -19,10 +20,21 @@ void dot_wmma4x4( half *__cuda_0,half *__cuda_1,half *__cuda_2)
 # 19 "main.cu"
 }
 # 1 "main.cudafe1.stub.c"
-void __device_stub__Z11fill_matrixP6__halfj( half *__par0,  const unsigned __par1) {  __cudaLaunchPrologue(2); __cudaSetupArgSimple(__par0, 0UL); __cudaSetupArgSimple(__par1, 8UL); __cudaLaunch(((char *)((void ( *)(half *, const unsigned))fill_matrix))); }
+void __device_stub__Z7mat_subP6__halfS0_S0_j( half *__par0,  half *__par1,  half *__par2,  const unsigned __par3) {  __cudaLaunchPrologue(4); __cudaSetupArgSimple(__par0, 0UL); __cudaSetupArgSimple(__par1, 8UL); __cudaSetupArgSimple(__par2, 16UL); __cudaSetupArgSimple(__par3, 24UL); __cudaLaunch(((char *)((void ( *)(half *, half *, half *, const unsigned))mat_sub))); }
 # 21 "main.cu"
-void fill_matrix( half *__cuda_0,const unsigned __cuda_1)
+void mat_sub( half *__cuda_0,half *__cuda_1,half *__cuda_2,const unsigned __cuda_3)
 # 22 "main.cu"
+{__device_stub__Z7mat_subP6__halfS0_S0_j( __cuda_0,__cuda_1,__cuda_2,__cuda_3);
+
+
+
+
+}
+# 1 "main.cudafe1.stub.c"
+void __device_stub__Z11fill_matrixP6__halfj( half *__par0,  const unsigned __par1) {  __cudaLaunchPrologue(2); __cudaSetupArgSimple(__par0, 0UL); __cudaSetupArgSimple(__par1, 8UL); __cudaLaunch(((char *)((void ( *)(half *, const unsigned))fill_matrix))); }
+# 29 "main.cu"
+void fill_matrix( half *__cuda_0,const unsigned __cuda_1)
+# 30 "main.cu"
 {__device_stub__Z11fill_matrixP6__halfj( __cuda_0,__cuda_1);
 
 
@@ -30,7 +42,7 @@ void fill_matrix( half *__cuda_0,const unsigned __cuda_1)
 
 }
 # 1 "main.cudafe1.stub.c"
-static void __nv_cudaEntityRegisterCallback( void **__T3) {  __nv_dummy_param_ref(__T3); __nv_save_fatbinhandle_for_managed_rt(__T3); __cudaRegisterEntry(__T3, ((void ( *)(half *, const unsigned))fill_matrix), _Z11fill_matrixP6__halfj, (-1)); __cudaRegisterEntry(__T3, ((void ( *)(half *, half *, half *))dot_wmma4x4), _Z11dot_wmma4x4P6__halfS0_S0_, (-1)); }
+static void __nv_cudaEntityRegisterCallback( void **__T3) {  __nv_dummy_param_ref(__T3); __nv_save_fatbinhandle_for_managed_rt(__T3); __cudaRegisterEntry(__T3, ((void ( *)(half *, const unsigned))fill_matrix), _Z11fill_matrixP6__halfj, (-1)); __cudaRegisterEntry(__T3, ((void ( *)(half *, half *, half *, const unsigned))mat_sub), _Z7mat_subP6__halfS0_S0_j, (-1)); __cudaRegisterEntry(__T3, ((void ( *)(half *, half *, half *))dot_wmma4x4), _Z11dot_wmma4x4P6__halfS0_S0_, (-1)); }
 static void __sti____cudaRegisterAll(void) {  __cudaRegisterBinary(__nv_cudaEntityRegisterCallback);  }
 
 #pragma GCC diagnostic pop
