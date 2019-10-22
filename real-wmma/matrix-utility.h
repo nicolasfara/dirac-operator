8a968711d04eb2092bb4e1f1916c218c00d9660b
nic0lasfara <nicolas.farabegoli@gmail.com>
2019-10-22 12:28:22 +0200

    Fix index

--- real-wmma/matrix-utility.h
+++ real-wmma/matrix-utility.h
@@ -39,9 +39,11 @@ void fill_tcu_matrix_half(half *h_ptr, const unsigned matrix_count)
 {
   for (unsigned z = 0; z < matrix_count; z++) {
     for (unsigned i = 0; i < 5; i++) {
-      for (unsigned j = 0; j < 144; j+=16) {
-        unsigned offset = (256*z)+(51*i);
-        h_ptr[offset+j] = __float2half(j);
+      for (unsigned j = 0; j < 3; j++) {
+        for (unsigned y = 0; y < 3; y++) {
+          unsigned offset = (256*z)+(51*i);
+          h_ptr[offset+j*16+y] = __float2half(j);
+        }
       }
     }
   }
