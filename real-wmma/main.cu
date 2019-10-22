30f4b2c9f5b7e14262dfb41f06fe219f718932cf
nic0lasfara <nicolas.farabegoli@gmail.com>
2019-10-22 12:33:25 +0200

    Test

--- real-wmma/main.cu
+++ real-wmma/main.cu
@@ -7,12 +7,19 @@
 int main(int argc, char **argv)
 {
   half *h_a;
-  allocate_cpu_side_tcu_matrix_half((void **)&h_a, 5);
-  fill_tcu_matrix_half(h_a, 5);
+  allocate_cpu_side_tcu_matrix_half((void **)&h_a, 10);
+  fill_tcu_matrix_half(h_a, 10);
 
   for (unsigned i = 0; i < 16; i++) {
     for (unsigned j = 0; j < 16; j++) {
       printf("%.1f\t", __half2float(h_a[j+i*16]));
+    }
+    printf("\n");
+  }
+
+  for (unsigned i = 0; i < 16; i++) {
+    for (unsigned j = 0; j < 16; j++) {
+      printf("%.1f\t", __half2float(h_a[256+j+i*16]));
     }
     printf("\n");
   }
