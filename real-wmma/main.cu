9882c424409f33d348d145e8d1ad039520faa2e9
Nicolas Farabegoli <nicolas.farabegoli@gmail.com>
2019-10-23 21:07:47 +0200

    TCU vs Normal vs CUBLAS

--- real-wmma/main.cu
+++ real-wmma/main.cu
@@ -1,49 +1,127 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <cublas_v2.h>
 #include "wmma-common.h"
 #include "matrix-utility.h"
 
-#define TCU_MAT 10
+#define TCU_MAT 40
 
 int main(int argc, char **argv)
 {
+  cudaEvent_t start, stop;
+  cudaEventCreate(&start);
+  cudaEventCreate(&stop);
+  float elapsed;
+  
+  half *h_a_tcu;
+  half *h_b_tcu;
+  half *h_c_tcu;
+  allocate_cpu_side_tcu_matrix_half(&h_a_tcu, TCU_MAT);
+  allocate_cpu_side_tcu_matrix_half(&h_b_tcu, TCU_MAT);
+  allocate_cpu_side_tcu_matrix_half(&h_c_tcu, TCU_MAT);
+  fill_zero_tcu_matrix(h_a_tcu, TCU_MAT);
+  fill_zero_tcu_matrix(h_b_tcu, TCU_MAT);
+  fill_zero_tcu_matrix(h_c_tcu, TCU_MAT);
+  fill_tcu_matrix_half(h_a_tcu, TCU_MAT);
+  fill_tcu_matrix_half(h_b_tcu, TCU_MAT);
+
+  //for (unsigned i = 0; i < 16; i++) {
+  //  for (unsigned j = 0; j < 16; j++) {
+  //    printf("%.1f\t", __half2float(h_a_tcu[j+i*16]));
+  //  }
+  //  printf("\n");
+  //}
+
+  half *d_a_tcu;
+  half *d_b_tcu;
+  half *d_c_tcu;
+  allocate_gpu_side_tcu_matrix_half((void **)&d_a_tcu, TCU_MAT);
+  allocate_gpu_side_tcu_matrix_half((void **)&d_b_tcu, TCU_MAT);
+  allocate_gpu_side_tcu_matrix_half((void **)&d_c_tcu, TCU_MAT);
+  copy_tcu_matrix_to_gpu_half(d_a_tcu, h_a_tcu, TCU_MAT);
+  copy_tcu_matrix_to_gpu_half(d_b_tcu, h_b_tcu, TCU_MAT);
+  copy_tcu_matrix_to_gpu_half(d_c_tcu, h_c_tcu, TCU_MAT);
+
+  cudaStream_t streams[TCU_MAT/5];
+  for (unsigned i = 0; i < TCU_MAT/5; i++)
+    cudaStreamCreate(&streams[i]);
+ 
+  cudaEventRecord(start, 0);
+
+  for (unsigned i = 0; i < TCU_MAT/5; i++)
+    dot_wmma16x16<<<1, 32, 0, streams[i]>>>(d_a_tcu+i*256, d_b_tcu+i*256, d_c_tcu+i*256);
+    //dot_wmma16x16<<<1, 32>>>(d_a_tcu+i*256, d_b_tcu+i*256, d_c_tcu+i*256);
+
+  cudaEventRecord(stop, 0);
+  cudaEventSynchronize(stop);
+  cudaEventElapsedTime(&elapsed, start, stop);
+  elapsed /= 1000.0f;
+  printf("TCU Version: %fs\n", elapsed);
+
+  copy_tcu_matrix_to_cpu_half(h_c_tcu, d_c_tcu, TCU_MAT);
+
+  ////////// End TCU version////////////////////////////////////////////
+  //////////////////////////////////////////////////////////////////////
+
   half *h_a;
   half *h_b;
   half *h_c;
-  allocate_cpu_side_tcu_matrix_half((void **)&h_a, TCU_MAT);
-  allocate_cpu_side_tcu_matrix_half((void **)&h_b, TCU_MAT);
-  allocate_cpu_side_tcu_matrix_half((void **)&h_c, TCU_MAT);
-  fill_tcu_matrix_half(h_a, TCU_MAT);
-  fill_tcu_matrix_half(h_b, TCU_MAT);
-  fill_zero_tcu_matrix(h_c, TCU_MAT);
-
+  allocate_cpu_side_matrix_half(&h_a, 3, 3, TCU_MAT);
+  allocate_cpu_side_matrix_half(&h_b, 3, 1, TCU_MAT);
+  allocate_cpu_side_matrix_half(&h_c, 3, 1, TCU_MAT);
+  
   half *d_a;
   half *d_b;
   half *d_c;
-  allocate_gpu_side_matrix_half((void **)&d_a, TCU_MAT);
-  allocate_gpu_side_matrix_half((void **)&d_b, TCU_MAT);
-  allocate_gpu_side_matrix_half((void **)&d_c, TCU_MAT);
-  copy_matrix_to_gpu_half(d_a, h_a, TCU_MAT);
-  copy_matrix_to_gpu_half(d_b, h_b, TCU_MAT);
-  copy_matrix_to_gpu_half(d_c, h_c, TCU_MAT);
 
-  dot_wmma16x16<<<1, 320>>>(d_a, d_b, d_c);
+  allocate_gpu_side_matrix_half((void **)&d_a, 3, 3, TCU_MAT);
+  allocate_gpu_side_matrix_half((void **)&d_b, 3, 1, TCU_MAT);
+  allocate_gpu_side_matrix_half((void **)&d_c, 3, 1, TCU_MAT);
+  copy_matrix_to_gpu_half(d_a, h_a, 3, 3, TCU_MAT);
+  copy_matrix_to_gpu_half(d_b, h_b, 3, 1, TCU_MAT);
+  copy_matrix_to_gpu_half(d_c, h_c, 3, 1, TCU_MAT);
 
-  copy_matrix_to_cpu_half(h_c, d_c, TCU_MAT);
+  cudaEventRecord(start, 0);
 
-  for (unsigned i = 0; i < 16; i++) {
-    for (unsigned j = 0; j < 16; j++) {
-      printf("%.1f\t", __half2float(h_c[j+i*16]));
-    }
-    printf("\n");
-  }
+  //for (unsigned i = 0; i < TCU_MAT; i++)
+  mat_vec_mul<<<1, TCU_MAT>>>(d_a, d_b, d_c);
 
-  for (unsigned i = 0; i < 16; i++) {
-    for (unsigned j = 0; j < 16; j++) {
-      printf("%.1f\t", __half2float(h_c[256+j+i*16]));
-    }
-    printf("\n");
-  }
+  cudaEventRecord(stop, 0);
+  cudaEventSynchronize(stop);
+  cudaEventElapsedTime(&elapsed, start, stop);
+  elapsed /= 1000.0f;
+  printf("Normal Version: %fs\n", elapsed);
 
+
+  //////////////////////// Test cublas full matrix /////////////////////////
+  half *d_a_c; 
+  half *d_b_c; 
+  half *d_c_c; 
+  cudaMalloc((void **)&d_a_c, sizeof(half)*120*120);
+  cudaMalloc((void **)&d_b_c, sizeof(half)*120*120);
+  cudaMalloc((void **)&d_c_c, sizeof(half)*120*120);
+
+  cublasHandle_t handle;
+  cublasCreate(&handle);
+  cublasSetMathMode(handle, CUBLAS_TENSOR_OP_MATH);
+  half alpha = __float2half(1.0f);
+  half beta = __float2half(0.0f);
+
+  cudaEventRecord(start, 0);
+
+  cublasGemmEx(handle, CUBLAS_OP_N, CUBLAS_OP_N, 120, 120, 120, &alpha, d_a_c, CUDA_R_16F, 120, d_b_c, CUDA_R_16F, 120, &beta, d_c_c, CUDA_R_16F, 120, CUDA_R_16F, CUBLAS_GEMM_DEFAULT_TENSOR_OP);
+
+  cudaEventRecord(stop, 0);
+  cudaEventSynchronize(stop);
+  cudaEventElapsedTime(&elapsed, start, stop);
+  elapsed /= 1000.0f;
+  printf("Cublas Version: %fs", elapsed);
+
+  free(h_a_tcu);
+  free(h_b_tcu);
+  free(h_c_tcu);
+  cudaFree(d_a_tcu);
+  cudaFree(d_b_tcu);
+  cudaFree(d_c_tcu);
   return EXIT_SUCCESS;
 }
--- real-wmma/matrix-utility.h
+++ real-wmma/matrix-utility.h
@@ -11,10 +11,28 @@
 #include <helper_cuda.h>
 #include <helper_functions.h>
 
-void allocate_cpu_side_matrix_half(void **h_ptr, const size_t rows, const size_t cols, const unsigned matrix_count)
+void allocate_cpu_side_matrix_half(half **h_ptr, const size_t rows, const size_t cols, const unsigned matrix_count)
 {
   // Allocate on CPU the matrix with allignment
-  posix_memalign(h_ptr, 128, rows*cols*matrix_count*sizeof(half));
+  *h_ptr = (half *) malloc(rows*cols*matrix_count*sizeof(half));
+}
+
+void allocate_gpu_side_matrix_half(void **d_ptr, const size_t rows, const size_t cols, const unsigned matrix_count)
+{
+  checkCudaErrors(cudaMalloc(d_ptr, rows*cols*matrix_count*sizeof(half)));
+  checkCudaErrors(cudaGetLastError());
+}
+
+void copy_matrix_to_cpu_half(half *h_ptr, half *d_ptr, unsigned rows, unsigned cols, const unsigned matrix_count)
+{
+  checkCudaErrors(cudaMemcpy(h_ptr, d_ptr, rows*cols*matrix_count*sizeof(half), cudaMemcpyDeviceToHost));
+  checkCudaErrors(cudaGetLastError());
+}
+
+void copy_matrix_to_gpu_half(half *d_ptr, half *h_ptr, unsigned rows, unsigned cols, const unsigned matrix_count)
+{
+  checkCudaErrors(cudaMemcpy(d_ptr, h_ptr, rows*cols*matrix_count*sizeof(half), cudaMemcpyHostToDevice));
+  checkCudaErrors(cudaGetLastError());
 }
 
 void fill_matrix_half(half *h_ptr, const size_t rows, const size_t cols, const unsigned matrix_count)
@@ -28,11 +46,38 @@ void fill_matrix_half(half *h_ptr, const size_t rows, const size_t cols, const u
   }
 }
 
-void allocate_cpu_side_tcu_matrix_half(void **h_ptr, const unsigned matrix_count)
+////////////////////////////////// TCU Version //////////////////////////////////////////////
+
+
+void allocate_cpu_side_tcu_matrix_half(half **h_ptr, const unsigned matrix_count)
 {
   const unsigned mat_16x16 = 16*16;
   const unsigned mat_16x16_count = matrix_count/5; //On each 16x16 matrix we could insert at most 5 matrix 3x3 on the diagonal of the big one.
-  posix_memalign(h_ptr, 128, mat_16x16*mat_16x16_count*sizeof(half));
+  *h_ptr = (half *) malloc(mat_16x16*mat_16x16_count*sizeof(half));
+}
+
+void allocate_gpu_side_tcu_matrix_half(void **d_ptr, const unsigned matrix_count)
+{
+  const unsigned mat_16x16 = 16*16;
+  const unsigned mat_16x16_count = matrix_count/5;
+  checkCudaErrors(cudaMalloc(d_ptr, mat_16x16*mat_16x16_count*sizeof(half)));
+  checkCudaErrors(cudaGetLastError());
+}
+
+void copy_tcu_matrix_to_cpu_half(half *h_ptr, half *d_ptr, const unsigned matrix_count)
+{
+  const unsigned mat_16x16 = 16*16;
+  const unsigned mat_16x16_count = matrix_count/5;
+  checkCudaErrors(cudaMemcpy(h_ptr, d_ptr, mat_16x16*mat_16x16_count*sizeof(half), cudaMemcpyDeviceToHost));
+  checkCudaErrors(cudaGetLastError());
+}
+
+void copy_tcu_matrix_to_gpu_half(half *d_ptr, half *h_ptr, const unsigned matrix_count)
+{
+  const unsigned mat_16x16 = 16*16;
+  const unsigned mat_16x16_count = matrix_count/5;
+  checkCudaErrors(cudaMemcpy(d_ptr, h_ptr, mat_16x16*mat_16x16_count*sizeof(half), cudaMemcpyHostToDevice));
+  checkCudaErrors(cudaGetLastError());
 }
 
 void fill_zero_tcu_matrix(half *h_ptr, const unsigned matrix_count)
@@ -57,73 +102,50 @@ void fill_tcu_matrix_half(half *h_ptr, const unsigned matrix_count)
   }
 }
 
-void allocate_gpu_side_matrix_half(void **d_ptr, const unsigned matrix_count)
-{
-  const unsigned mat_16x16 = 16*16;
-  const unsigned mat_16x16_count = matrix_count/5;
-  checkCudaErrors(cudaMalloc(d_ptr, mat_16x16*mat_16x16_count*sizeof(half)));
-  checkCudaErrors(cudaGetLastError());
-}
 
-void copy_matrix_to_cpu_half(half *h_ptr, half *d_ptr, const unsigned matrix_count)
-{
-  const unsigned mat_16x16 = 16*16;
-  const unsigned mat_16x16_count = matrix_count/5;
-  checkCudaErrors(cudaMemcpy(h_ptr, d_ptr, mat_16x16*mat_16x16_count*sizeof(half), cudaMemcpyDeviceToHost));
-  checkCudaErrors(cudaGetLastError());
-}
-
-void copy_matrix_to_gpu_half(half *d_ptr, half *h_ptr, const unsigned matrix_count)
-{
-  const unsigned mat_16x16 = 16*16;
-  const unsigned mat_16x16_count = matrix_count/5;
-  checkCudaErrors(cudaMemcpy(d_ptr, h_ptr, mat_16x16*mat_16x16_count*sizeof(half), cudaMemcpyHostToDevice));
-  checkCudaErrors(cudaGetLastError());
-}
+///////////////////////////////////// Kernels //////////////////////////////////////////
 
 __global__ void mat_vec_mul(half *matrix, half *in_vect, half *out_vect)
 {
-  half vec0 = in_vect[0];
-  half vec1 = in_vect[1];
-  half vec2 = in_vect[2];
+  const unsigned i = threadIdx.x + blockIdx.x*blockDim.x;
+  half vec0 = (in_vect+i*3)[0];
+  half vec1 = (in_vect+i*3)[1];
+  half vec2 = (in_vect+i*3)[2];
 
-  half mat00 = matrix[0];
-  half mat01 = matrix[1];
-  half mat02 = matrix[2];
+  half mat00 = (matrix+i*9)[0];
+  half mat01 = (matrix+i*9)[1];
+  half mat02 = (matrix+i*9)[2];
 
-  half mat10 = matrix[3];
-  half mat11 = matrix[4];
-  half mat12 = matrix[5];
+  half mat10 = (matrix+i*9)[3];
+  half mat11 = (matrix+i*9)[4];
+  half mat12 = (matrix+i*9)[5];
 
-  half mat20 = matrix[6];
-  half mat21 = matrix[7];
-  half mat22 = matrix[8];
+  half mat20 = (matrix+i*9)[6];
+  half mat21 = (matrix+i*9)[7];
+  half mat22 = (matrix+i*9)[8];
 
 //Multiply 3rd row by eta
   //mat20 = make_cuDoubleComplex(cuCreal(mat20)*eta, cuCimag(mat20)*eta);
   //mat21 = make_cuDoubleComplex(cuCreal(mat21)*eta, cuCimag(mat21)*eta);
   //mat22 = make_cuDoubleComplex(cuCreal(mat22)*eta, cuCimag(mat22)*eta);
 
-  out_vect[0] = mat00*vec0 + mat01*vec1 + mat02*vec2;
+  (out_vect+i*3)[0] = mat00*vec0 + mat01*vec1 + mat02*vec2;
 
-  out_vect[1] = mat10*vec0 + mat11*vec1 + mat12*vec2;
+  (out_vect+i*3)[1] = mat10*vec0 + mat11*vec1 + mat12*vec2;
 
-  out_vect[2] = mat20*vec0 + mat21*vec1 + mat22*vec2;
+  (out_vect+i*3)[2] = mat20*vec0 + mat21*vec1 + mat22*vec2;
 }
 
 __global__ void dot_wmma16x16(half *a, half *b, half *c)
 {
-  const unsigned i = threadIdx.x + blockDim.x * blockIdx.x;
-  const unsigned offset = i*256;
-
   wmma::fragment<wmma::matrix_a, 16, 16, 16, half, wmma::row_major> a_frag;
   wmma::fragment<wmma::matrix_b, 16, 16, 16, half, wmma::row_major> b_frag;
   wmma::fragment<wmma::accumulator, 16, 16, 16, half> c_frag;
-  wmma::load_matrix_sync(a_frag, a+offset, 16);
-  wmma::load_matrix_sync(b_frag, b+offset, 16);
+  wmma::load_matrix_sync(a_frag, a, 16);
+  wmma::load_matrix_sync(b_frag, b, 16);
   wmma::fill_fragment(c_frag, 0.0f);
   wmma::mma_sync(c_frag, a_frag, b_frag, c_frag);
-  wmma::store_matrix_sync(c+offset, c_frag, 16, wmma::mem_row_major);
+  wmma::store_matrix_sync(c, c_frag, 16, wmma::mem_row_major);
 }
 
 
