1a24ac9bcf743748b2668612de473481038e50c7
Nicolas Farabegoli <nicolas.farabegoli@gmail.com>
2019-10-23 15:41:17 +0200

    Add TCU matrix on CPU and GPU

--- real-wmma/main.cu
+++ real-wmma/main.cu
@@ -3,26 +3,30 @@
 #include "wmma-common.h"
 #include "matrix-utility.h"
 
+#define TCU_MAT 10
 
 int main(int argc, char **argv)
 {
   half *h_a;
-  allocate_cpu_side_tcu_matrix_half((void **)&h_a, 10);
-  fill_tcu_matrix_half(h_a, 10);
+  half *h_b;
+  half *h_c;
+  allocate_cpu_side_tcu_matrix_half((void **)&h_a, TCU_MAT);
+  allocate_cpu_side_tcu_matrix_half((void **)&h_b, TCU_MAT);
+  allocate_cpu_side_tcu_matrix_half((void **)&h_c, TCU_MAT);
+  fill_tcu_matrix_half(h_a, TCU_MAT);
+  fill_tcu_matrix_half(h_b, TCU_MAT);
+  fill_zero_tcu_matrix(h_c, TCU_MAT);
 
-  for (unsigned i = 0; i < 16; i++) {
-    for (unsigned j = 0; j < 16; j++) {
-      printf("%.1f\t", __half2float(h_a[j+i*16]));
-    }
-    printf("\n");
-  }
+  half *d_a;
+  half *d_b;
+  half *d_c;
+  allocate_gpu_side_matrix_half((void **)&d_a, TCU_MAT);
+  allocate_gpu_side_matrix_half((void **)&d_b, TCU_MAT);
+  allocate_gpu_side_matrix_half((void **)&d_c, TCU_MAT);
+  copy_matrix_to_gpu_half(d_a, h_a, TCU_MAT);
+  copy_matrix_to_gpu_half(d_b, h_b, TCU_MAT);
+  copy_matrix_to_gpu_half(d_c, h_c, TCU_MAT);
 
-  for (unsigned i = 0; i < 16; i++) {
-    for (unsigned j = 0; j < 16; j++) {
-      printf("%.1f\t", __half2float(h_a[256+j+i*16]));
-    }
-    printf("\n");
-  }
 
   return EXIT_SUCCESS;
 }
